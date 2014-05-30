cadae528de270f7ba6f3dc16992f8c307d92a332
xu354cjo1008 <xu354cjo1008@gmail.com>
2014-05-30 11:05:06 +0800

    Initial Commit

---
+++ AlgorithmProject/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  AlgorithmProject
+//
+//  Created by 李勁璋 on 2014/5/30.
+//  Copyright (c) 2014年 ntu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
